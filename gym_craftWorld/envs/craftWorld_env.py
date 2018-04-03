4b8d458a61135bc0ce1af66279e0b12a42b2a9ea
Shruti Bhanderi <frooty2308@gmail.com>
2018-04-03 16:27:53 -0400

    final env

---
+++ gym_craftWorld/envs/craftWorld_env.py
@@ -0,0 +1,15 @@
+import gym
+from gym import error, spaces, utils
+from gym.utils import seeding
+
+class craftWorldEnv(gym.Env):
+  metadata = {'render.modes': ['human']}
+
+  def __init__(self):
+    ###add here
+  def step(self, action):
+    ### add here
+  def reset(self):
+   ### add here
+  def render(self, mode='human', close=False):
+  ## add here
